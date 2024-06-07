5dbcbea20e600c580aa2090ea8b18cc5eb059a8e
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2024-06-07 16:19:18 +0300

    feat: ajouter class  verité qui return true

---
+++ src/main/java/Ratiocinator/Calculus/RatiocinatorCalculus/Verité.java
@@ -0,0 +1,11 @@
+package Ratiocinator.Calculus.RatiocinatorCalculus;
+
+public class Verité extends Affirmation {
+    public Verité(String texte) {
+        super(texte);
+    }
+    @Override
+    public boolean verité() {
+        return true;
+    }
+}
