3b83aa068198944003336fabf27dbcf03ffbe626
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2024-06-07 16:12:25 +0300

    first commit

---
+++ src/main/java/Ratiocinator/Calculus/RatiocinatorCalculus/RatiocinatorCalculusApplication.java
@@ -0,0 +1,13 @@
+package Ratiocinator.Calculus.RatiocinatorCalculus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatiocinatorCalculusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatiocinatorCalculusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ratiocinator/Calculus/RatiocinatorCalculus/RatiocinatorCalculusApplicationTests.java
@@ -0,0 +1,13 @@
+package Ratiocinator.Calculus.RatiocinatorCalculus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatiocinatorCalculusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
