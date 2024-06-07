3bbca394c328df8c6a1937e6d434c4fcb6633eba
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2024-06-07 16:22:09 +0300

    feat: ajouter la class conjoction et

---
+++ src/main/java/Ratiocinator/Calculus/RatiocinatorCalculus/conjoction/ConjoctionEt.java
@@ -0,0 +1,23 @@
+package Ratiocinator.Calculus.RatiocinatorCalculus.conjoction;
+
+
+import Ratiocinator.Calculus.RatiocinatorCalculus.Affirmation;
+
+public class ConjoctionEt extends Affirmation {
+    private final Affirmation affirmation1;
+    private final Affirmation affirmation2;
+
+    public ConjoctionEt(String texte,Affirmation affirmation1, Affirmation affirmation2) {
+        super(texte);
+        this.affirmation1 = affirmation1;
+        this.affirmation2 = affirmation2;
+    }
+
+    @Override
+    public boolean verité() {
+        if (affirmation1.verité()== ! (false || true) || affirmation2.verité() == !(false || true)){
+            return !(false || true);
+        }
+        return affirmation1.verité() && affirmation1.verité();
+    }
+}
